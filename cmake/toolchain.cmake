ec376227bdda64b07806b1f86b46b8d753ddc112
codeofcc <372502982@qq.com>
2026-07-29 18:00:18 +0800

    改回全量boost，采用下载压缩包解压的方式集成

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,12 +1,26 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm64)   # 改为 arm64
+# arm64-linux-gnu-toolchain.cmake
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
+SET(CMAKE_SYSTEM_NAME Linux CACHE STRING "toolchain default")
 
-set(SYSROOT /home/yanfa/ubuntu20-arm64-sysroot)
-set(TARGET_TRIPLE aarch64-linux-gnu)
+SET(CMAKE_SYSTEM_PROCESSOR aarch64 CACHE STRING "toolchain default")
 
-set(CMAKE_C_FLAGS "--target=${TARGET_TRIPLE} --sysroot=${SYSROOT}" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS "--target=${TARGET_TRIPLE} --sysroot=${SYSROOT}" CACHE STRING "" FORCE)
-set(CMAKE_EXE_LINKER_FLAGS "--target=${TARGET_TRIPLE} --sysroot=${SYSROOT}" CACHE STRING "" FORCE)
+SET(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc CACHE STRING "toolchain default")
+SET(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++ CACHE STRING "toolchain default")
+SET(CMAKE_AR /usr/bin/aarch64-linux-gnu-ar CACHE STRING "toolchain default")
+SET(CMAKE_C_COMPILER_AR /usr/bin/aarch64-linux-gnu-ar CACHE STRING "toolchain default")
+SET(CMAKE_CXX_COMPILER_AR /usr/bin/aarch64-linux-gnu-ar CACHE STRING "toolchain default")
+SET(CMAKE_RANLIB /usr/bin/aarch64-linux-gnu-ranlib CACHE STRING "toolchain default")
+SET(CMAKE_C_COMPILER_RANLIB /usr/bin/aarch64-linux-gnu-ranlib CACHE STRING "toolchain default")
+SET(CMAKE_CXX_COMPILER_RANLIB /usr/bin/aarch64-linux-gnu-ranlib CACHE STRING "toolchain default")
+SET(CMAKE_LINKER /usr/bin/aarch64-linux-gnu-ld CACHE STRING "toolchain default")
+SET(CMAKE_NM /usr/bin/aarch64-linux-gnu-nm CACHE STRING "toolchain default")
+SET(CMAKE_READELF /usr/bin/aarch64-linux-gnu-readelf CACHE STRING "toolchain default")
+SET(CMAKE_OBJCOPY /usr/bin/aarch64-linux-gnu-objcopy CACHE STRING "toolchain default")
+SET(CMAKE_OBJDUMP /usr/bin/aarch64-linux-gnu-objdump CACHE STRING "toolchain default")
+
+SET(CMAKE_SYSROOT /home/yanfa/ubuntu20-arm64-sysroot CACHE STRING "toolchain default")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER CACHE STRING "toolchain default")
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY CACHE STRING "toolchain default")
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY CACHE STRING "toolchain default")
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY CACHE STRING "toolchain default")
